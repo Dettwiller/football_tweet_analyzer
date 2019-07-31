13f73493f8360f8c0c6171c143bee0938b8166bb
Dettwiller <idd0790@gmail.com>
2019-07-31 09:43:17 -0500

    High level, basic framework for new, single function structure

---
+++ stream_and_analyze.py
@@ -0,0 +1,13 @@
+
+def forever_stream():
+    # TODO define a function to stream tweets
+    # TODO handle stream failures with while True or try block
+    pass
+
+
+
+if __name__ == "__main__":
+    # TODO: fork a stream to gather data
+    # TODO: loop through games and perform analysis if time_until_game < 1 hour
+    # NOTE: Games in clean_schedule.csv are in chronological order
+    pass
